70fbda843e0a13e27e6ae9c6f535f892e5ce7809
Adamskiee <tosinomatthew@gmail.com>
2026-09-09 18:18:04 +0800

    chore: setup esp32 environment and pin definitions

---
+++ firmware/main/Config.h
@@ -0,0 +1,26 @@
+#pragma once
+
+// Relay Pins (Active LOW)
+#define PIN_RELAY_SOLENOID 27
+#define PIN_RELAY_FAN 26
+#define PIN_RELAY_LED_RED 25
+#define PIN_RELAY_LED_YELLOW 33
+#define PIN_RELAY_LED_GREEN 14
+
+#define RELAY_ON LOW
+#define RELAY_OFF HIGH
+
+// Digital Sensors
+#define PIN_DHT22 32
+#define PIN_MAX6675_SCK 18
+#define PIN_MAX6675_SO 19
+#define PIN_MAX6675_CS 5
+
+// PM2.5 Serial (Hardware Serial 2)
+#define PIN_PM25_RX 16
+#define PIN_PM25_TX 17
+
+// Analog Sensors
+#define PIN_MQ135 34
+#define PIN_MQ2 35
+#define V_DIVIDER_RATIO 0.6 // 15K / (10K + 15K)
--- firmware/main/main.ino
+++ firmware/main/main.ino
@@ -1,7 +1,9 @@
+#include "Config.h"
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("System Booting...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
