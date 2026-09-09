1c292de41f4ca0711a5ed096c4d740a3db3abc94
Adamskiee <tosinomatthew@gmail.com>
2026-09-09 18:44:00 +0800

    fix: add MAX6675 library to dependency manifests

--- firmware/install_deps.sh
+++ firmware/install_deps.sh
@@ -7,4 +7,4 @@ arduino-cli core update-index --additional-urls https://raw.githubusercontent.co
 arduino-cli core install esp32:esp32@2.0.11 --additional-urls https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
 
 # Install required libraries
-arduino-cli lib install "Adafruit PM25 AQI Sensor@2.0.0" "DHT sensor library@1.4.7"
+arduino-cli lib install "Adafruit PM25 AQI Sensor@2.0.0" "DHT sensor library@1.4.7" "MAX6675 library@1.1.2"
